f333e54656b841ac204ceeed5cb22a8a41e97cac
danprophet <dan.averinn@gmail.com>
2022-09-16 15:47:44 +0300

    String instead of text

--- modules/purchases.js
+++ modules/purchases.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const PurchaseSchema = new Schema({
     customer_id : {type: Number},
     product_id : {type: Number},
-    product_name : {type: text},
+    product_name : {type: String},
     quantity : {type: Number}
 });
 
