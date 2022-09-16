cb3eca2be828858ab05e70ce644003d69f9eab1b
danprophet <dan.averinn@gmail.com>
2022-09-16 23:55:21 +0300

    update in purchase added async functions

--- modules/purchases.js
+++ modules/purchases.js
@@ -3,6 +3,8 @@ const Schema = mongoose.Schema;
 
 const PurchaseSchema = new Schema({
     customer_id : {type: Number},
+    year: {type: Number},
+    month: {type: Number},
     product_id : {type: Number},
     product_name : {type: String},
     quantity : {type: Number}
--- routes/api.js
+++ routes/api.js
@@ -37,19 +37,113 @@ router.get('/makePurchase', function(req, res, next){
     res.render('makePurchase');
 })
 
+// TODO: check in the cost DB if the product id exist OR/AND user ID exist in the users DB
 router.post('/makePurchase/done', function(req, res, next){
+    // add current date:
+    let currentDate = new Date();
+    req.body['year'] = currentDate.getFullYear();
+    req.body['month'] = currentDate.getMonth();
+
     Purchase.create(req.body).then(function (purchase){
         console.log(purchase);
         res.render('DoneMakePurchase', {product_id:purchase.product_id, product_name:purchase.product_name})
     }).catch(next);
 })
 
-// Get Data Mechanism:
+// 4. Report
+router.get('/report', function(req, res, next){
+    res.render('Report');
+})
+
+router.post('/report/done', function(req, res, next){
+    // Purchase.create(req.body).then(function (report){
+    //     console.log(report);
+    const month = req.body.month;
+    const year = req.body.year;
+    const current_user_id = req.body.customer_id;
+
+    // TODO: check if the user exists in the database (lines 71-77 is the find function - find function return empty list if the user not found)
+    function checkExistanceOfData()
+    {
+        return new Promise(resolve => {
+            if ((month >= 1 && month <= 12) && (year >= 2000))
+            {
+                User.find({"id":current_user_id}).then(function(result)
+                {
+                    resolve();
+                }).catch((data) =>
+                {
+                    res.render('error', {message: "user not found in database"});
+                });
+            }
+            else
+            {
+                res.render('error', {message: "Year or Month not valid"});
+            }
+
+        });
+    }
+
+    async function getData()
+    {
+        const asyncResult = await checkExistanceOfData();
+        // Purchase.find({"customer_id": req.body.customer_id}).then (function(cost)
+        // {
+        //     console.log(cost);
+        //     res.render('index');
+        // }).catch(next)
+
+        User.find({"id":current_user_id}).then(function(result)
+        {
+            // resolve();
+            console.log(result)
+            if (result.length === 0)
+            {
+                res.render('error', {message: "NO USER"});
+            }
+            else
+            {
+                res.render('error', {message: "ITS OK"});
+            }
+
+        }).catch((data) =>
+        {
+            res.render('error', {message: "user not found in database"});
+        });
+    }
+    getData().catch(next);
+        // 1. check validity of data
+        // if ((report.month >= 1 && report.month <= 12) && (report.year >= 2000))
+        // {
+        //     // 1.1 search for the relevent data in this time for this user
+        //     // need to fetch data from purchases DB
+        //
+        // }
+
+
+
+        // Purchase.find({"purchase.year": report.year,
+        //                     "purchase.month": report.month,
+        //                     "purchase.customer_id": report.id}).toArray(function)
+        // Purchase.find({"purchase.customer_id": report.id}).to
+    // Purchase.find({"customer_id": req.body.customer_id}).then (function(cost)
+    // {
+    //     console.log(cost)
+    //     res.render('index');
+    // }).catch(next)
+
+
+    //     res.render('DoneMakePurchase', {product_id:purchase.product_id, product_name:purchase.product_name})
+    // }).catch(next);
+})
+
+// Test:
 router.get('/currentUser', function(req, res, next) {
     // res.send("current user: "+req.query.id);
     res.render('index');
 });
 
+// TODO: Make this a central HTML page with menu to all sub pages/forms
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
